ab810da3789dd19d49e7197eb11db0bc713d6f8d
Dinesh Metkari <dajee.metkari@stackroute.in>
2018-06-19 12:18:09 +0530

    ic

---
+++ src/main/java/com/stackroute/Greeter.java
@@ -0,0 +1,7 @@
+package com.stackroute;
+
+public class Greeter {
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
---
+++ src/test/java/hello/GreeterTest.java
@@ -0,0 +1,19 @@
+package hello;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.stackroute.Greeter;
+
+public class GreeterTest {
+	
+	private Greeter greeter = new Greeter();
+
+	@Test
+	public void greeterSaysHello() {
+		assertThat(greeter.sayHello(), containsString("Hello"));
+	}
+
+}
